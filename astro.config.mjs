1618f720e5610abe839418d5abad261ff6cc146b
Tan Jerowe <86645548+JeroTan@users.noreply.github.com>
2026-05-03 16:45:53 +0800

    feat: added cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: cloudflare(),
   integrations: [react(), sitemap()],
 
   vite: {
